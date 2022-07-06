cf0929bf0548604c5f38df503e31d0d128d9ff6b
Dener <denerpuhl@gmail.com>
2022-07-06 19:52:59 -0300

    added port

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    logOverride: { "this-is-undefined-in-esm": "silent" },
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     logOverride: { "this-is-undefined-in-esm": "silent" },
+//   },
+//   server: {
+//     port: loadEnv(...process.env):,
+//   },
+// })
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") }
+
+  return defineConfig({
+    plugins: [react()],
+    esbuild: {
+      logOverride: { "this-is-undefined-in-esm": "silent" },
+    },
+    server: {
+      port: Number(process.env.PORT),
+    },
+  })
+}
